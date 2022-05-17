76141d65d83c47d6fb97c893893bfacfc4bd0737
Richard Jones <rich@linxplicable.org>
2022-05-17 14:52:46 +0100

    python 3; kodi-x11

--- plugin.program.steamlink/resources/lib/start.sh
+++ plugin.program.steamlink/resources/lib/start.sh
@@ -3,6 +3,6 @@
 DIRNAME=$( dirname "${BASH_SOURCE[0]}" )
 
 sudo openvt -c 7 -s -f -- su kodi -c "bash ${DIRNAME}/heartbeat.sh & /usr/bin/steamlink &"
-sudo systemctl stop kodi
+sudo systemctl stop kodi-x11
 
 exit
